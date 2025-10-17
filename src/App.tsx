19e5a6c78eb5830203481746031107144f97a0e3
Tony Onodi <tonyonodi@gmail.com>
2025-10-17 01:13:59 +0100

    Changed title, changed file icon, added server copy.

--- src/App.tsx
+++ src/App.tsx
@@ -316,18 +316,11 @@ function App() {
         >
           <div className="drop-zone-content">
             <svg
-              className="upload-icon"
               xmlns="http://www.w3.org/2000/svg"
-              fill="none"
               viewBox="0 0 24 24"
-              stroke="currentColor"
+              fill="currentColor"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-              />
+              <path d="M15 4V8H19V20H5V4H15ZM3.9985 2C3.44749 2 3 2.44405 3 2.9918V21.0082C3 21.5447 3.44476 22 3.9934 22H20.0066C20.5551 22 21 21.5489 21 20.9925L20.9997 7L16 2H3.9985ZM15.0008 11.667L10.1219 8.41435C10.0562 8.37054 9.979 8.34717 9.9 8.34717C9.6791 8.34717 9.5 8.52625 9.5 8.74717V15.2524C9.5 15.3314 9.5234 15.4086 9.5672 15.4743C9.6897 15.6581 9.9381 15.7078 10.1219 15.5852L15.0008 12.3326C15.0447 12.3033 15.0824 12.2656 15.1117 12.2217C15.2343 12.0379 15.1846 11.7895 15.0008 11.667Z"></path>
             </svg>
             <p>Drop a video file here</p>
             <p className="or-text">or</p>
@@ -337,6 +330,7 @@ function App() {
             >
               Select File
             </button>
+            <p>Your file stays on your device and is never uploaded to a server</p>
             <input
               ref={fileInputRef}
               type="file"
